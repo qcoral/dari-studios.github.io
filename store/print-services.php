2e049948aae9312c8f75c3c4d9c67164e03d023b
Dari <Dari@dari.com>
2021-05-13 16:24:45 -0400

    Revert "Revert "Merge pull request #3 from LongHappyFrog/master""
    This reverts commit 5a7a6eba93ac719083400b457225ca8ada65f522.

---
+++ store/print-services.php
@@ -0,0 +1,18 @@
+<?php
+$_GET['titleName'] = 'Print Services';
+$ROOT = "../";
+include($ROOT.'include/head.php')
+?>
+<div id="store-content">
+<body id="store-body">
+        <h1 id="store-header">3D Printer services</h1>
+        <h1 id="store-text-yellow">$2 /hr (final price may change)</h1>   
+        <p id="store-text-yellow">if you want to buy anything please reach out to me on <a href="/socials/" id="store-text-yellow">discord or twitter </a>
+        <p id="store-text-white"> 
+        Got a custom model you want to print? No worries! I can 3D print any model you want (check with me on printability first). DM me on discord preferably and we can work things out. <br>
+        </p>
+        <p id="store-text-pink">Available 3D printer colours: Gold, Orange, Pale blue.</p>
+    </body>
+        <a href="index.php" id="store-link">back to store</a>
+</div>
+</html>
