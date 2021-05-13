a536f65f8b83278de0dfdb41f4ebefa800513d35
Dari <Dari@dari.com>
2021-05-13 16:24:54 -0400

    Revert "PHP Update"
    This reverts commit 28d9b7f57cbfa106daad1b427c4a15cb1a8ef64b.

--- include/head.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>َDari's Website <?php echo $_GET['titleName']?></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo $ROOT;?>style/style.css">
-  </head>
